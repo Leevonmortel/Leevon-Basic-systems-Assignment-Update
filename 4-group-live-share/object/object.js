9e46cbfbe9b5335c37f850c04cea8112fb29c87a
Leevonmortel <113203643+Leevonmortel@users.noreply.github.com>
2022-10-24 11:37:44 -0700

    Add files via upload

---
+++ 4-group-live-share/object/object.js
@@ -0,0 +1,10 @@
+var student = {};
+
+    firstName = "Mark";
+    lastName =  "Jacob";
+    age = 31;
+    id =  "A001234";
+
+ var sentence = `The new employee ${firstName} ${lastName} is ${age} years old. The employee ID is ${id}.`;
+
+ console.log(sentence);
